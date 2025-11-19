4f534d9e850d23346743a73f55d60535df341e85
FileEX <kongali@interia.pl>
2025-11-19 17:26:57 +0100

    Add flags argument

--- Client/game_sa/CModelInfoSA.cpp
+++ Client/game_sa/CModelInfoSA.cpp
@@ -1528,7 +1528,7 @@ void CModelInfoSA::StaticResetModelAnimations()
         if (modelInfo)
         {
             modelInfo->DisableObjectAnimation(false);
-            modelInfo->SetObjectAnimation(nullptr, 0);
+            modelInfo->SetObjectAnimation(nullptr, 0, 0);
         }
 
         CBaseModelInfoSAInterface* modelInfoInterface = modelInfo->GetInterface();
@@ -2394,6 +2394,7 @@ RpClump* __fastcall CClumpModelInfoSAInterface::CreateInstance(CClumpModelInfoSA
     {
         CAnimBlendHierarchySAInterface* anim = nullptr;
         auto* modelInfo = pGame->GetModelInfo(static_cast<CBaseModelInfoSAInterface*>(clumpModelInfo));
+        eAnimationFlags                 flags = ANIMATION_IS_LOOPED;
 
         if (clumpModelInfo->m_nAnimFileIndex != -1)
         {
@@ -2407,12 +2408,15 @@ RpClump* __fastcall CClumpModelInfoSAInterface::CreateInstance(CClumpModelInfoSA
             }
         }
         else if (modelInfo)
+        {
             anim = modelInfo->GetObjectAnimation();
+            flags = modelInfo->GetObjectAnimationFlags();
+        }
 
         if (anim)
         {
             RpAnimBlendClumpInit(clump);
-            pGame->GetAnimManager()->BlendAnimation(clump, anim, ANIMATION_IS_LOOPED, 1.0f);
+            pGame->GetAnimManager()->BlendAnimation(clump, anim, flags, 1.0f);
         }
     }
 
